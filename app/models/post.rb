d583d620e48bec6992e5f16472f732c0bdb0394a
nicolaspiper <nicolaspiper@yahoo.com>
2019-08-21 22:42:36 -0700

    make progress on ActiveStorage

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,5 +10,5 @@
 #
 
 class Post < ApplicationRecord
-    has_many_attached :photos
+    has_one_attached :photo
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'posts/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
     resources :users, only:[:create]
     resource :session, only:[:create, :destroy]
   end
+  resources :posts, only: :show
   root :to => "static_pages#root";
+
 end
