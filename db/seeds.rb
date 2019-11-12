fe705cff94a38fd9e747bdaf622571165301c092
nicolaspiper <nicolaspiper@yahoo.com>
2019-11-11 23:33:25 -0800

    update seeds file, need to add more, and have .DS_Store from attempting to dump db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+Post.delete_all
+
+users = User.create([{ username: 'demoUser', name: 'demoUser', email: 'demoUser@demo.com', password: 'password'},
+                    { username: 'gooseMan', name: 'AJ', email: 'topgungoose@hotmail.com', password: 'starwars'},
+                    { username: 'Haberman', name: 'Fritz Haber', email: 'fritz@empire.de', password: 'nitrogen'},
+                    { username: 'calculusTho', name: 'Isaac Newton', email: 'apple@apple.org', password: 'derivative'}])
+
+posts = Post.create([{caption: '', author_id: , created_at: Time.now(), updated_at: Time.now()},
+                    {caption: '', author_id: , created_at: Time.now(), updated_at: Time.now()},
+                    {caption: '', author_id: , created_at: Time.now(), updated_at: Time.now()},
+                    {caption: '', author_id: , created_at: Time.now(), updated_at: Time.now()}
+                    ])
