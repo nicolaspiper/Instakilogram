0ecb34144e473aab92a168508bc24db8970244be
nicolaspiper <nicolaspiper@yahoo.com>
2019-11-15 23:54:18 -0800

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,9 +26,29 @@ users.each do |user|
 end
 
 posts = [
-{caption: '', author_id: },
-{caption: '', author_id: },
-{caption: '', author_id: },
-{caption: '', author_id: }
+{caption: "Look again at that dot. That's here. That's home. That's us.", author_id: 7},
+{caption: "88, another one.", author_id: 5},
+{caption: "I just had an idea, it's as if it fell out of the sky", author_id: 4},
+{caption: "The unleashed power of the atom has changed everything except our thinking.", author_id: 6},
+{caption: "Bring back the wooly mamoth!", author_id: 10},
+{caption: "Mix, mix, stir, stir. One more ingredient and I shall have immortality!", author_id: 8},
+{caption: "Invented a process today, felt cute, snapped a pic", author_id: 3},
+{caption: "Beer just got a whole lot better", author_id: 4},
+{caption: "Come at me Superman, got your Kryptonite right here", author_id: 5},
+{caption: "What if I was moving super fast away from this clock, would time slow down for me? Lmk what you think", author_id: 6},
+{caption: "Can't believe someone stole my idea for a username 'TH3_M1CR0SC0P3R_F4N4T1C'! No matter, I looked at some pondwater samples today along with some other… fluids.", author_id: 11},
+{caption: "84. Discovered a really heavy metal today. I'll name it after my home country", author_id: 5},
+{caption: "spontaneous generation is hogwash, fight me.", author_id: 3},
+{caption: '', author_id: 4},
+{caption: '', author_id: 2},
+{caption: '', author_id: 1}
 ]
+posts_photos = [
+    ,
+    ,
+    ,
+    ,
+    ,
+    'https://instakilogram-pro.s3-us-west-1.amazonaws.com/alchemist.jpg',
+] 
 # # created_at: Time.now(), updated_at: Time.now()
