accd4d692c38516abf4345ee9eccf66ad3a9f03e
nicolaspiper <nicolaspiper@yahoo.com>
2019-11-19 13:45:17 -0800

    modify posts index a bit, leave comments for stuff to do

--- frontend/components/posts/posts.jsx
+++ frontend/components/posts/posts.jsx
@@ -14,20 +14,24 @@ class Posts extends React.Component {
         let all_posts = this.props.posts.map((post) => (
             <div className="postContainer" key={post.id}>
                 <div className="userPostHeader">
-                    <div className="profileImgMask">
-                        <img src={post.userPhoto} ></img>
+                    <div className="colorBorder">
+                        <div className="profileImgMask">
+                            <img src={post.userPhoto} ></img>
+                        </div>
                     </div>
-                    <p>{post.user}</p>
+                    <p id="username">{post.user}</p>
                     {/* <p>{User.find_by(post.author_id).username}</p> need to get posts frontend working*/}
                 </div>
                 <div className="postPhoto">
                     <img src={post.photoUrl} alt={post.caption} />
                 </div>
                 <div className="actionIcons">
+                    {/* weird font displayed in this placeholder because text is in div not a p */}
                     buttons go here
                 </div>
+                {/* PUT CONDITIONALLY DISPLAYED LIKES p tag here! */}
                 <div className="postCaption">
-                    <p>{post.user}</p>
+                    <p id="username">{post.user}</p>
                     <p>{post.caption}</p>
                 </div>
                 <div className="comments">
