36ab15d71d6057d24b0dcd34158f9241520fef55
nicolaspiper <nicolaspiper@yahoo.com>
2019-11-25 18:17:33 -0800

    update post actions for show post

--- frontend/actions/post_actions.js
+++ frontend/actions/post_actions.js
@@ -1,12 +1,22 @@
 import * as PostAPI from './../utils/post_api_util';
 
 export const LOAD_INITIAL_POSTS = "LOAD_INITIAL_POSTS";
+export const SHOW_POST = "SHOW_POST";
 
 export const loadInitialPosts = (posts) => ({
     type: LOAD_INITIAL_POSTS,
     posts
 })
 
+export const showPost = (post) => ({
+    type: SHOW_POST,
+    post
+})
+
 export const getPosts = () => dispatch =>  (
     PostAPI.fetchPosts().then((posts) => dispatch(loadInitialPosts(posts)))
+);
+
+export const getPost = (id) => dispatch =>  (
+    PostAPI.fetchPost(id).then((post) => dispatch(showPost(post)))
 );
