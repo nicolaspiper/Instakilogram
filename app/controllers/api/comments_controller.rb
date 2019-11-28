ab82fbb191b3462b4d98669c8093ae70d8e5a4d1
nicolaspiper <nicolaspiper@yahoo.com>
2019-11-27 16:21:24 -0800

    files initted when using rails generation

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,15 @@
+class Api::CommentsController < ApplicationController
+
+    def show
+    end
+
+    def edit
+    end
+
+    def destroy
+    end
+
+    def create
+    end
+    
+end
--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,12 +1,12 @@
 class Api::PostsController < ApplicationController
   
     def index
-        @posts = Post.includes(:user).order(:created_at).first(10) # get the first ten posts on first load
+        @posts = Post.includes(:user, :comments).order(:created_at).first(10) # get the first ten posts on first load
         render :index
     end
 
     def show
-        @post = Post.find(params[:id])
+        @post = Post.includes(:user,:comments).find(params[:id])
         # create failure conditional, if there is a 500 level server error, i.e. someone 
         # guesses a url they will conditionally channeled certain ways, if not logged in but guessing
         # they will be prompted to sign up or log in the demoUser to post, if they
