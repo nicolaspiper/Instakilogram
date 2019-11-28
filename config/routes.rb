f15d603c7ed973baa4dbb4e78e03fa9aa58fbdb2
nicolaspiper <nicolaspiper@yahoo.com>
2019-11-27 16:13:21 -0800

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only:[:create]
     resource :session, only:[:create, :destroy]
-    resources :posts, only: [:index, :show, :create, :destroy, :update]
+    resources :posts, only: [:index, :show, :create, :destroy, :edit]
+    resources :comments, only: [:show, :create, :destroy, :edit]
   end
   root :to => "static_pages#root";
 
