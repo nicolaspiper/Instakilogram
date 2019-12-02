79585e114b465f90b09c87bce82c85adc68237c7
nicolaspiper <nicolaspiper@yahoo.com>
2019-12-02 13:18:10 -0800

    will have to make show user page for each user

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -9,6 +9,11 @@ class Api::UsersController < ApplicationController
         end
     end
 
+    # def show
+    #     @user = User.find(params[:username])
+    #     render "api/users/show"
+    # end
+
     private
 
     def user_params
